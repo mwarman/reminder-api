a70d6da0a4b04b43a9e617e85fe4782100e2c185
Matt Warman <warmanmr@gmail.com>
2017-10-23 07:58:00 -0400

    MySQL Connection Configuration. Added Spring Profiles.

--- src/test/java/com/example/reminderapi/service/ReminderServiceTests.java
+++ src/test/java/com/example/reminderapi/service/ReminderServiceTests.java
@@ -10,12 +10,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.reminderapi.model.Reminder;
 import com.example.reminderapi.repository.ReminderRepository;
 
+@ActiveProfiles("ci")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReminderServiceTests {
--- src/test/java/com/example/reminderapi/service/UserServiceTests.java
+++ src/test/java/com/example/reminderapi/service/UserServiceTests.java
@@ -8,12 +8,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.reminderapi.model.User;
 import com.example.reminderapi.repository.UserRepository;
 
+@ActiveProfiles("ci")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTests {
